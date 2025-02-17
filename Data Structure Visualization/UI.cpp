5de82466b652a50ef2ca20589c17f15b8bde38a8
LeDatPhucAn <2015phucan@gmail.com>
2025-02-17 14:03:29 +0700

    Update UI.cpp

---
+++ Data Structure Visualization/UI.cpp
@@ -0,0 +1,26 @@
+#include "UI.h"
+#include "LinkedList.h"
+#include <string>
+void UI::drawnode(node* head, int X, int Y, int r) {
+	Font font = GetFontDefault();
+	float fontSize = 40.0f;
+	float spacing = 2.0f;
+	std::string messageStr = std::to_string(head->data);
+	const char* message = messageStr.c_str();
+	// Measure text dimensions
+	Vector2 textSize = MeasureTextEx(font, message, fontSize, spacing);
+
+	//draw the node
+	DrawCircle(X, Y, r, BLACK);
+	DrawCircle(X, Y, r - 15, RAYWHITE);
+	DrawText(message, X - textSize.x / 2, Y - textSize.y / 2, 40, BLUE);
+}
+void UI::LinkedListVSL(LinkedList* list) {
+	node* cur = list->head;
+	int i = 1;
+	while (cur) {
+		drawnode(cur, 100 * i, 100, 50);
+		i += 2;
+		cur = cur->next;
+	}
+}

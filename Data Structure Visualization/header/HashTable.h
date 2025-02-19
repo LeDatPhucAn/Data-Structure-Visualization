e17b158816a9e2cd39235a9bfc1c1fd24287791b
Khoa Nguyen <anhkhoa102006@gmail.com>
2025-02-19 13:18:07 +0700

    Add hash table funtion

--- Data Structure Visualization/header/HashTable.h
+++ Data Structure Visualization/header/HashTable.h
@@ -1,1 +1,14 @@
 #pragma once
+#include <vector>
+class HashTable {
+private:
+    std::vector<std::vector<int>> data;
+    int size;
+
+public:
+    HashTable(int s);
+    void insertHashTable(int value);
+    bool findHashTable(int value);
+    void deleteHashTable(int value);
+    void display();
+};

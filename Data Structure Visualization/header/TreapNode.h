12eb9e7cb5cabe89e6f5fcb0aa8cfb29403ce7fc
Pham Tien Dat <ptdat2422@apcs.fitus.edu.vn>
2025-02-19 16:33:23 +0700

    Minor changes in "TreapNode.h"

--- Data Structure Visualization/header/TreapNode.h
+++ Data Structure Visualization/header/TreapNode.h
@@ -2,13 +2,14 @@
 #include "Node.h"
 
 template <typename T>
-class TreeNode : public Node<T> {
+class TreapNode : public Node<T> {
 public:
-    TreeNode<T>* left;
-    TreeNode<T>* right;
+    int priority;
+    TreapNode<T>* left;
+    TreapNode<T>* right;
 
-    TreeNode(T value) : Node<T>(value), left(nullptr), right(nullptr) {}
-    ~TreeNode() override {
+    TreapNode(T value) : Node<T>(value), priority(rand()), left(nullptr), right(nullptr) {}
+    ~TreapNode() override {
         delete left;
         delete right;
     }

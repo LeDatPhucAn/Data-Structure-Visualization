f32cc7d05ec9e676502f1d21a38962d0644f05ed
LeDatPhucAn <2015phucan@gmail.com>
2025-03-12 09:04:04 +0700

    adding node variables

--- Data Structure Visualization/header/Node.h
+++ Data Structure Visualization/header/Node.h
@@ -1,10 +1,14 @@
 #pragma once
 #include <iostream>
+#include <raylib.h>
+
 using namespace std;
 
 class Node {
 public:
     int data;
+    //Vector2 position;
+
     Node(int value) : data(value) {}
     virtual ~Node() {}
 };

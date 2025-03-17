68dd6f5daf81ae14bcf1a6d8154618523640b005
Khoa Nguyen <anhkhoa102006@gmail.com>
2025-03-17 22:12:12 +0700

    Add initial function and buttons for HashTableUI.cpp

--- Data Structure Visualization/source/HashTableUI.cpp
+++ Data Structure Visualization/source/HashTableUI.cpp
@@ -9,6 +9,50 @@ void HashTableUI::init() {
     insertHashTable(15);
     insertHashTable(7);
     insertHashTable(12);
+
+    Button::insertHeadButton(Buttons, new TextBox("Insert", 100, UI::screenHeight * 3 / 4));
+    Button* Value = new TextBox("Value:");
+    Button* ValueInput = new NumberInputBox(3);
+    Button* Enter = new TextBox(">");
+
+    Buttons[0]->insertSubButton(Value);
+    Buttons[0]->insertSubButton(ValueInput);
+    Buttons[0]->insertSubButton(Enter, [this, ValueInput]() {
+        int value = ValueInput->getNumber();
+        if (value >= 0) {
+            insertHashTable(value);
+        }
+        });
+
+    Button::insertHeadButton(Buttons, new TextBox("Remove"));
+    Button* Value1 = new TextBox("Value:");
+    Button* ValueInput1 = new NumberInputBox(3);
+    Button* Enter1 = new TextBox(">");
+    Buttons[1]->insertSubButton(Value1);
+    Buttons[1]->insertSubButton(ValueInput1);
+    Buttons[1]->insertSubButton(Enter1, [this, ValueInput1]() {
+        int value = ValueInput1->getNumber();
+        if (value >= 0) {
+            deleteHashTable(value);
+        }
+        });
+
+    Button::insertHeadButton(Buttons, new TextBox("Search"));
+    Button* Value2 = new TextBox("Value:");
+    Button* ValueInput2 = new NumberInputBox(3);
+    Button* Enter2 = new TextBox(">");
+    Buttons[2]->insertSubButton(Value2);
+    Buttons[2]->insertSubButton(ValueInput2);
+    Buttons[2]->insertSubButton(Enter2, [this, ValueInput2]() {
+        int value = ValueInput2->getNumber();
+        bool found = findHashTable(value);
+        cout << "Value " << value << " found: " << (found ? "Yes" : "No") << endl;
+        });
+
+    Buttons.push_back(new TextBox("Menu", 50, 50));
+    Buttons[3]->onClick = [this]() {
+        this->scenehandler->changeScene(MENU);
+    };
 }
 
 void HashTableUI::drawHashTable() {

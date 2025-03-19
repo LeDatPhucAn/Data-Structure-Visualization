12ba9a2d53f59902afbec1e0983b27bde4e69d32
ntd <lqd.tk22.ngotandat@gmail.com>
2025-03-19 20:45:19 +0700

    Revert "Merge remote-tracking branch 'origin' into TanDat"
    This reverts commit cb09bb9028b79488ed14203507468ee5b992df0b, reversing
    changes made to 644534bca74dd7c60858f3da3a97edabeb05dfd6.

--- Data Structure Visualization/header/Treap.h
+++ Data Structure Visualization/header/Treap.h
@@ -43,7 +43,6 @@ public:
 		clear(root);
 		root = nullptr;
 	}
-	TreapNode* getRoot();
 	int getSubtreeWidth(TreapNode* curr);
 	void updateSubtreeWidth(TreapNode* curr);
 	TreapNode* insert(TreapNode* root, Vector2 pos, int key, int priority);

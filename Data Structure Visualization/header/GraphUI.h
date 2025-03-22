8af8aec646140f4d070c3f376d41bb7016875134
ntd <lqd.tk22.ngotandat@gmail.com>
2025-03-23 04:37:30 +0700

    Mergr branh 'PhucAn' into 'TanDat'

--- Data Structure Visualization/header/GraphUI.h
+++ Data Structure Visualization/header/GraphUI.h
@@ -4,18 +4,23 @@
 #include "Button.h"
 #include "Graph.h" // Assuming you have this class
 
-class GraphUI : public SceneManager, public Graph {
+class GraphUI : public SceneManager {
+
 protected:
     vector<Button*> CodeBlocks;
     vector<Button*> buttonsOnGraph;
+
+    Graph* graph = nullptr;
 public:
-    GraphUI() : Graph(0) {
+    GraphUI() {
+        graph = new Graph();
+        init();
+    }
+    GraphUI(Graph* g) : graph(g) {
         init();
     }
 
-    
     void init() override;
-    void drawEdge();
     void initButtons() override;
     void updateButtonPositions() override {};
     void updateScene() override;
@@ -26,6 +31,13 @@ public:
 
     void drawGraph();
     //void drawNodeOnGraph(Node* node, Vector2 position);
-    ~GraphUI() {}
+    ~GraphUI() {
+        delete graph;
+        for (Button* btn : buttonsOnGraph) delete btn;
+        buttonsOnGraph.clear();
+        for (Button* codeBlock : CodeBlocks) delete codeBlock;
+        CodeBlocks.clear();
+    }
+    void clear();
 
 };

971b36f06f0568ebd2abe7751fadeea7a47919bc
Khoa Nguyen <anhkhoa102006@gmail.com>
2025-04-05 22:52:27 +0700

    use LLNode.h instead

--- Data Structure Visualization/header/HashTable.h
+++ Data Structure Visualization/header/HashTable.h
@@ -1,12 +1,12 @@
 #pragma once
-#include "HashTableNode.h"
+#include "LLNode.h"
 #include "Edge.h"
 #include <vector>
 #include "AnimationManager.h"
 using namespace std;
 class HashTable {
 public:
-    vector<HashTableNode*> buckets;
+    vector<LLNode*> buckets;
     static vector<CBEdge*> Edges;
     AnimationManager animManager;
     int bucketCount;
@@ -14,7 +14,7 @@ public:
     HashTable(int size = 5);
     ~HashTable();
 
-    void adjustPos(HashTableNode* head, int bucketIdx);
+    void adjustPos(LLNode* head, int bucketIdx);
     void insertNode(int x);
     void randomInsert(int x);
     bool remove(int x);

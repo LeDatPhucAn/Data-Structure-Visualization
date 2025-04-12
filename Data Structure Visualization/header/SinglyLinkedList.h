82ac79700802aee292b22eabf950c8cea943049e
LeDatPhucAn <2015phucan@gmail.com>
2025-04-12 13:21:31 +0700

    insert,remove,search animations are kinda good now

--- Data Structure Visualization/header/SinglyLinkedList.h
+++ Data Structure Visualization/header/SinglyLinkedList.h
@@ -17,6 +17,7 @@ public:
     }
     void adjustPos(LLNode* head);
     void adjustPosWithAnim(AnimationManager& animManager, LLNode* head);
+    void adjustPosWithAnim2(AnimationManager& animManager, LLNode* head);
     void insertnode(AnimationManager& animManager, int x, int pos);
     void randominsert(int x, int pos);
     void loadFromFile();

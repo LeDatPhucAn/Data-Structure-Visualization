54c360ed52f58bd8a6b69e232faafd6bc193b6bb
LeDatPhucAn <2015phucan@gmail.com>
2025-04-12 10:25:17 +0700

    updated adjust Pos to have animation

--- Data Structure Visualization/header/SinglyLinkedList.h
+++ Data Structure Visualization/header/SinglyLinkedList.h
@@ -16,6 +16,7 @@ public:
         clear();
     }
     void adjustPos(LLNode* head);
+    void adjustPosWithAnim(AnimationManager& animManager, LLNode* head);
     void insertnode(AnimationManager& animManager, int x, int pos);
     void randominsert(int x, int pos);
     void loadFromFile();

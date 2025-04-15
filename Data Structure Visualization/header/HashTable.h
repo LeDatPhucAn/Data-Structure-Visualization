0243cb81ba7078dd079cf7612bf964eca82ccb80
Khoa Nguyen <anhkhoa102006@gmail.com>
2025-04-15 18:06:33 +0700

    Update HashTable.h

--- Data Structure Visualization/header/HashTable.h
+++ Data Structure Visualization/header/HashTable.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include "AnimationManager.h"
 using namespace std;
+
 class HashTable {
 public:
     vector<LLNode*> buckets;
@@ -15,13 +16,16 @@ public:
     ~HashTable();
 
     void adjustPos(LLNode* head, int bucketIdx);
-    void insertNode(int x);
+    void adjustPosWithAnim(AnimationManager& animManager, LLNode* head, int bucketIdx);
+    void insertNode(vector<RectButton*>& CodeBlocks, AnimationManager& animManager, int x);
     void randomInsert(int x);
-    bool remove(int x);
+    bool remove(vector<RectButton*>& CodeBlocks, AnimationManager& animManager, int x);
     bool removeFromBucket(int x, int bucketIdx);
-    bool search(int x);
+    bool search(vector<RectButton*>& CodeBlocks, AnimationManager& animManager, int x);
     void clear();
+    void clearIndicates();
     void resize(int newSize);
+    void loadFromFile(vector<RectButton*>& CodeBlocks, AnimationManager& animManager);
     int hashFunction(int x) { return x % bucketCount; }
     void deleteEdges();
 };
